d7ab2d17b25a0d7a7d82361f365f62a8f92fd16b
Jade Forlani-Brennan <jade.forlani.brennan@gmail.com>
2022-09-11 13:39:51 +0800

    :sparkles: added iterative method

--- binarysearch/src/main.rs
+++ binarysearch/src/main.rs
@@ -1,25 +1,49 @@
 fn main() {
-    let nums: Vec<i32> = (1..10000).collect();
-    let target: i32 = 7001;
+    let nums: Vec<i32> = (1..=100).collect();
+    let target: i32 = 77;
 
-    println!("{}",nums.len());
-    println!("{:?}", nums[0]);
+    println!("array has: {} elements",nums.len());
+    println!("first element of array at index 0 is: {:?}", nums[0]);
 
-    let result: i32 = bruteforce(nums, target);
+// let result: i32 = bruteforce(nums, target);
+    let result:i32 = iterative(&nums,target);
 
-    println!("{}",result)
+    println!("index is: {:?} and value at index = {} which is equal to target ({})", result, nums[(result as usize)], target)
 }
 
-fn bruteforce(nums: Vec<i32>, target: i32) -> i32 {
-    let mut result: i32 = -1;
-    for i in 0..nums.len() - 1  { 
-        if nums[i] == target {
-            result = i as i32;
+// fn bruteforce(nums: Vec<i32>, target: i32) -> i32 {
+//     let mut result: i32 = -1;
+//     for i in 0..nums.len() - 1  { 
+//         if nums[i] == target {
+//             result = i as i32;
+//         }
+//     }
+//     return result;
+// }
+
+fn iterative(nums: &Vec<i32>, target: i32) -> i32{
+    let mut low:usize = 1;
+    let mut high:usize = nums.len();
+    let mut mid:usize;
+
+    while low <= high {
+        mid = low +  ((high - low) / 2);
+
+        if nums[mid] < target {
+            low = mid + 1;
         }
-    }
-    return result;
-}
 
+        if nums[mid] > target {
+            high = mid - 1;
+        }
+
+        if nums[mid] == target {
+            return mid as i32;
+        }
+
+    }
+    return -1;
+}
 // impl Solution {
 //     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
         
