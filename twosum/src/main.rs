492753d2dd65a8f058f90a76939d5a926dc4e53e
Jade Forlani-Brennan <jade.forlani.brennan@gmail.com>
2022-10-12 16:13:34 +0900

    :sparkles: added hashmap solution

--- twosum/src/main.rs
+++ twosum/src/main.rs
@@ -1,16 +1,55 @@
+//use std::collections::HashMap;
+
 fn main() {
-    let nums:Vec<i32> = vec![1,2,3,4,5,6,7,8,9,10];
-    let target:i32 = 19;
-    let result = brute_force(nums, target);
+    let nums:Vec<i32> = vec![3,2,4];
+    let target:i32 = 6;
+    let result = hash_daddy(nums,target);
     println!("index1: {:?}, index2: {:?}", result[0], result[1]);
 }
 
 
-// pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-//     // Returns the indicies in nums which add to target
-//     return vec![-1];
+
+pub fn hash_daddy(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    // Create a hash map and insert each item
+    let nums_len = nums.len();
+    let mut hash: std::collections::HashMap<i32, i32> = std::collections::HashMap::with_capacity(nums_len);
+    
+    // Insert each element into the hash map
+    // We will be looking up by the number itself later
+    // So we use the number as the key (not the index!)
+    for (idx, value) in nums.iter().enumerate() {
+        hash.insert(*value, idx as i32);
+    }
+
+    // Now loop through again
+    // And for each check if the other half of the sum exists in the hash map
+    // If it does pull it out and we're done!
+
+    for (idx, value) in nums.iter().enumerate() {
+        let compliment = target - value;
+
         
-// }
+        match hash.get(&compliment) {
+            // Found
+            Some(&compliment_idx) => {
+                // Cannot use the same element twice
+                if idx == compliment_idx as usize {
+                    continue;
+                } else {
+                    return vec![idx as i32, compliment_idx as i32];
+                }
+                
+            }
+            // Not found
+            None => {
+                continue;
+            }
+        }
+    }
+
+    unreachable!("dear god how did this happen")
+
+}
 
 pub fn brute_force(nums: Vec<i32>, target: i32) -> Vec<i32> {
     // Tries every possible combination N^N complexity worst case
